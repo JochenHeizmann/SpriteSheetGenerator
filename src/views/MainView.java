9285b032b3b0dff564561c641887c07285e8ef88
Dennis Simon <dennisosimon@gmail.com>
2011-02-21 23:01:52 +0100

    Added drag and drop support

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -2,6 +2,10 @@ package views;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
@@ -10,7 +14,10 @@ import java.awt.dnd.DropTargetListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -37,13 +44,14 @@ import util.Converter;
 import model.Model;
 
 public class MainView {
-	
+
 	private JFrame frame;
 	private Model model;
 	private final JPanel panelSpritesheet = new JPanel();
 	private final JFileChooser chooser = new JFileChooser();
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	
+	private FileFilter filter;
+
 	/**
 	 * Create the application.
 	 */
@@ -61,58 +69,60 @@ public class MainView {
 		frame.setBounds(100, 100, 400, 626);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		chooser.setDialogType(JFileChooser.OPEN_DIALOG);
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		chooser.setMultiSelectionEnabled(true);
-		chooser.setFileFilter(new FileNameExtensionFilter("Image files", "png", "gif", "jpg", "jpeg", "tiff", "tga"));
-		
-		panelSpritesheet.setBorder(new TitledBorder(null, "Preview", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		chooser.setFileFilter(new FileNameExtensionFilter("Image files", "png",
+				"gif", "jpg", "jpeg", "tiff", "tga"));
+
+		panelSpritesheet.setBorder(new TitledBorder(null, "Preview",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelSpritesheet.setBounds(0, 286, 400, 205);
 		frame.getContentPane().add(panelSpritesheet);
 		panelSpritesheet.setLayout(null);
-		
+
 		final Spritesheet canvas = new Spritesheet();
-		
+
 		final JSpinner spinnerFps = new JSpinner();
 		spinnerFps.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
-				canvas.setFps((Integer)spinnerFps.getValue());
+				canvas.setFps((Integer) spinnerFps.getValue());
 			}
 		});
 		spinnerFps.setModel(new SpinnerNumberModel(30, 1, 120, 1));
 		spinnerFps.setBounds(338, 171, 52, 28);
 		panelSpritesheet.add(spinnerFps);
-		
+
 		canvas.setBounds(10, 20, 380, 151);
 		canvas.setImages(model.getImages());
 		panelSpritesheet.add(canvas);
-		
+
 		JLabel lblFramerate = new JLabel("Framerate");
 		lblFramerate.setBounds(264, 177, 62, 16);
 		panelSpritesheet.add(lblFramerate);
-		
+
 		JPanel panelFiles = new JPanel();
-		panelFiles.setBorder(new TitledBorder(null, "Images", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelFiles.setBorder(new TitledBorder(null, "Images",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelFiles.setBounds(0, 6, 400, 268);
 		frame.getContentPane().add(panelFiles);
 		panelFiles.setLayout(null);
-		
-		
+
 		final JList listFiles = new JList(model.getListModel());
 		listFiles.setFont(new Font("Lucida Grande", Font.PLAIN, 8));
 		listFiles.setBounds(6, 18, 388, 203);
 		panelFiles.add(listFiles);
 		listFiles.setBorder(new LineBorder(new Color(0, 0, 0)));
-		
+
 		JButton buttonAdd = new JButton("Add");
 		buttonAdd.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				chooser.setVisible(true);
 				int returnVal = chooser.showOpenDialog(null);
-				
-				if(returnVal == JFileChooser.APPROVE_OPTION) {
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
 					try {
 						model.addFiles(chooser.getSelectedFiles());
 					} catch (IOException e1) {
@@ -124,7 +134,7 @@ public class MainView {
 		});
 		buttonAdd.setBounds(6, 233, 117, 29);
 		panelFiles.add(buttonAdd);
-		
+
 		JButton btnRemoveAll = new JButton("Remove all");
 		btnRemoveAll.addMouseListener(new MouseAdapter() {
 			@Override
@@ -135,13 +145,13 @@ public class MainView {
 		});
 		btnRemoveAll.setBounds(126, 233, 117, 29);
 		panelFiles.add(btnRemoveAll);
-		
+
 		JButton buttonRemove = new JButton("Remove selected");
 		buttonRemove.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				model.removeFiles(listFiles.getSelectedIndices());
-				if(model.getListModel().getSize() == 0) {
+				if (model.getListModel().getSize() == 0) {
 					canvas.repaint();
 				} else {
 					canvas.updateCanvas();
@@ -150,13 +160,14 @@ public class MainView {
 		});
 		buttonRemove.setBounds(245, 233, 149, 29);
 		panelFiles.add(buttonRemove);
-		
+
 		JScrollPane scrollPane = new JScrollPane(listFiles);
 		scrollPane.setBounds(5, 20, 389, 201);
 		panelFiles.add(scrollPane);
-		
+
 		JPanel panelSave = new JPanel();
-		panelSave.setBorder(new TitledBorder(null, "Save Spritesheet", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelSave.setBorder(new TitledBorder(null, "Save Spritesheet",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panelSave.setBounds(0, 503, 400, 101);
 		frame.getContentPane().add(panelSave);
 		panelSave.setLayout(null);
@@ -166,36 +177,40 @@ public class MainView {
 		buttonGroup.add(rdbtnHorizontal);
 		rdbtnHorizontal.setBounds(78, 26, 98, 23);
 		panelSave.add(rdbtnHorizontal);
-		
+
 		final JRadioButton rdbtnVertical = new JRadioButton("Vertical");
 		buttonGroup.add(rdbtnVertical);
 		rdbtnVertical.setBounds(188, 26, 79, 23);
 		panelSave.add(rdbtnVertical);
-		
+
 		JRadioButton rdbtnQuadratic = new JRadioButton("Quadratic");
 		buttonGroup.add(rdbtnQuadratic);
 		rdbtnQuadratic.setBounds(271, 26, 93, 23);
 		panelSave.add(rdbtnQuadratic);
-		
+
 		JButton buttonSave = new JButton("Save Spritesheet");
 		buttonSave.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				int returnVal = chooser.showSaveDialog(null);
-				if(returnVal == JFileChooser.APPROVE_OPTION) {
-					System.out.println(chooser.getSelectedFile().getAbsoluteFile());
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					System.out.println(chooser.getSelectedFile()
+							.getAbsoluteFile());
 					Converter.Format format;
-					
-					if(rdbtnHorizontal.isSelected()) {
+
+					if (rdbtnHorizontal.isSelected()) {
 						format = Converter.Format.HORIZONTAL;
-					} else if(rdbtnVertical.isSelected()) {
+					} else if (rdbtnVertical.isSelected()) {
 						format = Converter.Format.VERTICAL;
 					} else {
 						format = Converter.Format.QUADRATIC;
 					}
-					
+
 					try {
-						ImageIO.write(Converter.convert(model.getImages(), format), "png", new File(chooser.getSelectedFile().getAbsoluteFile().toString()));
+						ImageIO.write(
+								Converter.convert(model.getImages(), format),
+								"png", new File(chooser.getSelectedFile()
+										.getAbsoluteFile().toString()));
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -205,53 +220,75 @@ public class MainView {
 		});
 		buttonSave.setBounds(225, 61, 146, 29);
 		panelSave.add(buttonSave);
-		
+
 		JCheckBox chckbxSaveXmlFoo = new JCheckBox("Export Template");
 		chckbxSaveXmlFoo.setEnabled(false);
 		chckbxSaveXmlFoo.setBounds(78, 62, 135, 23);
 		panelSave.add(chckbxSaveXmlFoo);
-		
+
 		JLabel lblFormat = new JLabel("Format");
 		lblFormat.setBounds(16, 30, 61, 16);
 		panelSave.add(lblFormat);
-		
+
 		JLabel lblTemplate = new JLabel("Template");
 		lblTemplate.setBounds(16, 66, 61, 16);
 		panelSave.add(lblTemplate);
-		
+
 		new DropTarget(frame, new DropTargetListener() {
-			
+
 			@Override
 			public void dropActionChanged(DropTargetDragEvent dtde) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void drop(DropTargetDropEvent dtde) {
-				// TODO Auto-generated method stub
-				
+				Transferable t = dtde.getTransferable();
+
+				if (t.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+					dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+					try {
+						List list = (List) t.getTransferData(DataFlavor.javaFileListFlavor);
+						ArrayList<File> files = new ArrayList<File>();
+						for (int i = 0; i < list.size(); i++) {
+							File file = (File) list.get(i);
+							String filename = file.getName();
+							if(filename.endsWith("png") || filename.endsWith("gif") || filename.endsWith("jpg") || filename.endsWith("jpeg") || filename.endsWith("tiff") || filename.endsWith("tga")) {
+								files.add(file);
+							}
+						}
+						File[] f = new File[files.size()];
+						model.addFiles(files.toArray(f));
+
+					} catch (UnsupportedFlavorException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
-			
+
 			@Override
 			public void dragOver(DropTargetDragEvent dtde) {
-				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void dragExit(DropTargetEvent dte) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void dragEnter(DropTargetDragEvent dtde) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 		frame.setVisible(true);
 	}
 }
